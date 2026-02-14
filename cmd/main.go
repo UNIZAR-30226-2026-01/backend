71891097d129b6748bab9481c1103f5c79e95703
MimiMc4 <miguelmarclave@gmail.com>
2026-02-14 13:41:08 +0100

    mini mini prueba de gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
-import (
-	"log"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	log.Println("Hola laser chess")
+	router := gin.Default()
+	router.GET("/ping", getHello)
+
+	router.Run() // escucha en 0.0.0.0:8080 por defecto
+}
+
+func getHello(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
